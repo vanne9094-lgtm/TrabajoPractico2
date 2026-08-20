3ce56c23cafc38ee6d7b16596bd005e58ae9358b
Joaquín <joa.viarruel@gmail.com>
2026-08-20 19:26:08 -0300

    Update Castor

--- TrabajoPractico2/src/trabajopractico2/Castor.java
+++ TrabajoPractico2/src/trabajopractico2/Castor.java
@@ -8,6 +8,37 @@ package trabajopractico2;
  *
  * @author vanne
  */
-public class Castor {
-    
+public abstract class Castor {
+
+    protected double cola;
+    protected double velocidad;
+
+    public Castor() {
+    }
+
+    public Castor(double cola, double velocidad) {
+        this.cola = cola;
+        this.velocidad = velocidad;
+    }
+
+    public double getCola() {
+        return cola;
+    }
+
+    public void setCola(double cola) {
+        this.cola = cola;
+    }
+
+    public double getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(double velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public abstract void nadar();
+
+    public abstract void tocarGuitarra();
+
 }
