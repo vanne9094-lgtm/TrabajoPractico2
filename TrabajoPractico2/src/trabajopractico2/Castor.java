284c5d5fa45ccf33c2bb39ed68a938494688735b
Joaquín Viarruel <joa.viarruel@gmail.com>
2026-08-20 19:31:19 -0300

    Update Castor.java

--- TrabajoPractico2/src/trabajopractico2/Castor.java
+++ TrabajoPractico2/src/trabajopractico2/Castor.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package trabajopractico2;
 
-/**
- *
- * @author vanne
- */
 public abstract class Castor {
 
     protected double cola;
